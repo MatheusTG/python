9025a81234d51d052965bf3d2194e719e3c2104f
Matheus Teodoro <101060803+MatheusTG@users.noreply.github.com>
2022-03-20 09:29:18 -0300

    Teste

--- PythonExercicios/ex072.py
+++ PythonExercicios/ex072.py
@@ -7,12 +7,14 @@ while True:
     if -1 < c <= 20:
         print(f'O número que você digitou foi {tupla[c]}!')
         cont = ' '
+
         while cont not in 'SN':
             cont = str(input('Você quer continuar? [S/N] ')).strip().upper()[0]
             if cont not in 'SN':
                 print('Tente novamente. ', end='')
+
     if cont in 'N':
         print('Foi bom trabalhar com você!')
         break
     else:
-        print('Tente novamente!', end=' ')
+        print('Blz então vamos continuar!', end=' ')
